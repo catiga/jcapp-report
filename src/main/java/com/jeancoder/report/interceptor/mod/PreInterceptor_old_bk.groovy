ddad8ad2e599d236050b3661b39c406efad5ec25
jackielee <jackielee@192.168.0.100>
2019-07-01 11:15:07 +0800

    修改报表左侧菜单逻辑

--- src/main/java/com/jeancoder/report/interceptor/mod/PreInterceptor_old_bk.groovy
+++ src/main/java/com/jeancoder/report/interceptor/mod/PreInterceptor_old_bk.groovy
@@ -1,59 +1,51 @@
 package com.jeancoder.report.interceptor.mod
 
+import java.util.List
+import java.util.Map
+
 import com.jeancoder.annotation.urlmapped
+import com.jeancoder.annotation.urlpassed
 import com.jeancoder.app.sdk.JC
 import com.jeancoder.core.http.JCRequest
 import com.jeancoder.core.util.JackSonBeanMapper
-import com.jeancoder.jdbc.JcTemplate
 import com.jeancoder.report.ready.dto.sys.AppFunction
+import com.jeancoder.report.ready.dto.sys.SysFunction
 import com.jeancoder.report.ready.dto.sys.SysProjectInfo
 import com.jeancoder.report.ready.helper.GlobalHolder
-import com.jeancoder.report.ready.report.entity.SysReportDesign
 import com.jeancoder.report.ready.util.FuncUtil
 import com.jeancoder.report.ready.util.NativeUtil
 
 @urlmapped("/")
 
-//def mod_g = FuncUtil.build(1, '报表管理', null, '', 'fa-shopping-cart');
-//def mod_g1 = FuncUtil.build(101, '会员充值报表', 1, 'index', 'fa-shopping-cart',2);
-//def mod_g2 = FuncUtil.build(102, '商品网售售卖报表', 1, 'online_goods/index', 'fa-shopping-cart',2);
-//def mod_g3 = FuncUtil.build(103, '商品收银台报表', 1, 'commodity_register/index', 'fa-shopping-cart',2);
-//def mod_g4 = FuncUtil.build(104, '票务网售售卖报表', 1, 'ticketing_online/index', 'fa-shopping-cart',2);
-//def mod_g5 = FuncUtil.build(105, '票务收银台报表', 1, 'ticketing_counter/index', 'fa-shopping-cart',2);
-//def mod_g6 = FuncUtil.build(106, '会员购买报表', 1, 'members_buy/index', 'fa-shopping-cart',2);
-//def mod_g7 = FuncUtil.build(107, '票务预约报表', 1, 'ticketing_reserve/index', 'fa-shopping-cart',2);
 
-def mod_g1 = FuncUtil.build(1, '报表管理', null, 'cashier', 'fa-money');
-def mod_g1_1 = FuncUtil.build(101, '报表工具', 1, 'report/mgr', '', 2);
-def mod_g1_2 = FuncUtil.build(102, '报表预览', 1, 'report/list/list', '', 2);
+def mod_g = FuncUtil.build(1, '报表管理', null, '', 'fa-shopping-cart');
+def mod_g1 = FuncUtil.build(101, '会员充值报表', 1, 'index', 'fa-shopping-cart',2);
+def mod_g2 = FuncUtil.build(102, '商品网售售卖报表', 1, 'online_goods/index', 'fa-shopping-cart',2);
+def mod_g3 = FuncUtil.build(103, '商品收银台报表', 1, 'commodity_register/index', 'fa-shopping-cart',2);
+def mod_g4 = FuncUtil.build(104, '票务网售售卖报表', 1, 'ticketing_online/index', 'fa-shopping-cart',2);
+def mod_g5 = FuncUtil.build(105, '票务收银台报表', 1, 'ticketing_counter/index', 'fa-shopping-cart',2);
+def mod_g6 = FuncUtil.build(106, '会员购买报表', 1, 'members_buy/index', 'fa-shopping-cart',2);
+def mod_g7 = FuncUtil.build(107, '票务预约报表', 1, 'ticketing_reserve/index', 'fa-shopping-cart',2);
+def mod_2 = FuncUtil.build(2, '报表', null, 'cashier', 'fa-money');
+def mod_2_1 = FuncUtil.build(201, '报表工具', 2, 'report/mgr', '', 2);
+def mod_2_2 = FuncUtil.build(202, '报表列表', 2, 'report/list/list', '', 2);
 
 
-def result = [mod_g1_1,mod_g1_2];
+
+def result = [mod_g,mod_g1,mod_g2,mod_g3,mod_g4,mod_g5,mod_g6,mod_g7,mod_2,mod_2_1,mod_2_2];
 AppFunction mod_g_main = new AppFunction();
 mod_g_main.func_name = '报表管理';
-mod_g1_1.func_info = '快速制作业务报表工具';
-mod_g1_2.func_info = '已制作报表预览';
-
+mod_g1.func_info = '可查看会员充值报表的详细信息';
+mod_g2.func_info = '可查看商品网售售卖报表的详细信息';
+mod_g3.func_info = '可查看商品收银台报表的详细信息';
+mod_g4.func_info = '可查看票务网售售卖报表的详细信息';
+mod_g5.func_info = '可查看票务收银台报表的详细信息';
+mod_g6.func_info = '可查看会员购买报表的详细信息';
+mod_g7.func_info = '可查看票务预约报表的详细信息';
 def appFun=[];
-appFun.addAll([mod_g1]);
+appFun.addAll([mod_g]);
 def appFunChild=[];
-appFunChild.addAll([mod_g1_1, mod_g1_2]);
-
-
-//获取当前项目可用系统报表
-def mod_g2 = FuncUtil.build(2, '报表列表', null, 'cashier', 'fa-money');
-List<SysReportDesign> all_reports = JcTemplate.INSTANCE().find(SysReportDesign, 'select * from SysReportDesign where flag!=? and proj_id=?', -1, GlobalHolder.proj.id);
-if(all_reports) {
-	def i = 200;
-	for(x in all_reports) {
-		i++;
-		def mod_g2_tmp = FuncUtil.build(i, x.report_name, 2, 'report/report/list/detail/?report_no=' + x.report_no, '', 2);
-		result.add(mod_g2_tmp);
-	}
-}
-
-
-
+appFunChild.addAll([mod_g1, mod_g2,mod_g3,mod_g4,mod_g5,mod_g6,mod_g7]);
 JCRequest request = JC.request.get();
 request.setAttribute("appMain", mod_g_main);
 request.setAttribute('appFun', appFun);
