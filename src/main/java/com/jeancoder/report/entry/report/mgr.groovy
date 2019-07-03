f0d47a19df594ff4601e2a4a1160b38f721db632
jackielee <jackielee@192.168.123.129>
2019-07-03 09:32:33 +0800

    c

--- src/main/java/com/jeancoder/report/entry/report/mgr.groovy
+++ src/main/java/com/jeancoder/report/entry/report/mgr.groovy
@@ -3,6 +3,7 @@ package com.jeancoder.report.entry.report
 import com.jeancoder.app.sdk.JC
 import com.jeancoder.core.result.Result
 import com.jeancoder.jdbc.JcPage
+import com.jeancoder.jdbc.JcTemplate
 import com.jeancoder.report.ready.helper.GlobalHolder
 import com.jeancoder.report.ready.report.entity.SysReportDesign
 import com.jeancoder.report.ready.report.service.SysReportDesignService
@@ -31,7 +32,21 @@ try {
 } catch(any) { ps = 20;}
 
 SysReportDesignService sysReportDesignService = SysReportDesignService.INSTANCE;
-JcPage<SysReportDesign> page = sysReportDesignService.getDesignPage(GlobalHolder.proj.id.toLong(), ps, pn);
+//JcPage<SysReportDesign> page = sysReportDesignService.getDesignPage(GlobalHolder.proj.id.toLong(), ps, pn);
+
+JcPage<SysReportDesign> page = new JcPage<SysReportDesign>();
+page.setPs(ps);
+page.setPn(pn);
+String hql = "select * from SysReportDesign a where 1=1";
+def params = [];
+if(GlobalHolder.proj.root==1) {
+	hql += ' and flag!=?';
+	params.add(-1);
+} else {
+	hql += ' and proj_id = ? and flag <> ?';
+	params.add(GlobalHolder.proj.id, -1);
+}
+page = JcTemplate.INSTANCE().find(SysReportDesign, page, hql, params.toArray());
 
 Result result = new Result().setView("report/pdrreport_index");
 result.addObject('page', page);
